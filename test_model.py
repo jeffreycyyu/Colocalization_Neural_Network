fa329cde9212ba38dbb7be9978acbc549d5c3c08
Jeffrey C.Y. Yu <83930692+jeffreycyyu@users.noreply.github.com>
2022-05-07 11:59:37 -0400

    new simulated data

--- test_model.py
+++ test_model.py
@@ -1,20 +1,74 @@
-import os
-import math, sys
+import math
+import sys
 import numpy as np
 import pandas as pd
+from scipy.signal import argrelextrema
+import random
 import matplotlib.pyplot as plt
+from pandas import DataFrame as df
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Activation, Add, Conv1D, Conv1DTranspose, LSTM, Layer, LayerNormalization, ReLU, Embedding, Bidirectional
+import tensorflow_probability as tfp
+from tensorflow.keras.layers import Input, Embedding, Activation, Add, Conv1D, Conv1DTranspose, LSTM, Layer, LayerNormalization, ReLU, Embedding, Bidirectional
+from keras.preprocessing.sequence import pad_sequences
 import tensorflow_addons as tfa
 from tensorflow.keras.models import Model, Sequential
 from keras.utils.vis_utils import plot_model
+import edward2 as ed
+import warnings
 
-#Model Input
-#generate data 400 gene segments each of length 100bp/loci and with 5 traits/cell-types
-#test: GWAS, eQTL1, eQTL2, eQTL3, eQTL4 p-values for 100 bp's (100 loci/positions)
-#randomly generate 5 vectors which represent the 5 trait's p-values for 100 loci each
-specify_input_shape = (400, 100, 5)
-test_input = np.random.random(specify_input_shape)
+
+#MODEL INPUT
+
+#set path to 'data' folder
+sys.path.insert(0, '/Users/jeffreyyu/Documents/Sladek/colocalization_neural_network/data')
+
+#import function to read, zero pad, and positionally encode a tab delimited .txt data file
+from ipynb.fs.full.zero_padding_positional_encoding import IMPORT_ZERO_PAD_POSITIONAL_ENCODE
+
+#set random seed
+random.seed(25252)
+
+#data file pathname
+pathname = '/Users/jeffreyyu/Documents/Sladek/colocalization_neural_network/data/simulated_gwas.txt'
+
+#read, zero pad, and positionally encode
+test_input = IMPORT_ZERO_PAD_POSITIONAL_ENCODE(pathname)
+#input shape
+input_shape = test_input.shape
+
+
+#print input shape and input
+print(test_input.shape)
+print(test_input)
+
+
+
+
+#create a dataframe version of the input for viewing
+test_dataframe = pd.read_csv(pathname, sep='\t')
+test_dataframe.columns.name = None
+print(test_dataframe)
+#open plots in a seperate window for matplotlibx
+%matplotlib qt
+#example plot of first 1000 positions for all 3 traits with p-values on the -log10 scale (like in a manhattan plot)
+plt.scatter(test_dataframe['Position'][0:1000], -1*np.log10(test_dataframe['trait_1'][0:1000]), s=1)
+plt.scatter(test_dataframe['Position'][0:1000], -1*np.log10(test_dataframe['trait_2'][0:1000]), s=1)
+plt.scatter(test_dataframe['Position'][0:1000], -1*np.log10(test_dataframe['trait_3'][0:1000]), s=1)
+plt.show()
+
+
+# #Old simulated input (do not use)
+# #Model Input
+# #generate data 400 gene segments each of length 100bp/loci and with 5 traits/cell-types
+# #test: GWAS, eQTL1, eQTL2, eQTL3, eQTL4 p-values for 100 bp's (100 loci/positions)
+# #randomly generate 5 vectors which represent the 5 trait's p-values for 100 loci each
+# input_shape = (400, 100, 5)
+# test_input = np.random.random(input_shape)
+
+
+
+
+
 
 
 #MULTI_HEAD_ATTENTION
@@ -26,7 +80,7 @@ multihead_layer_test = MULTI_HEAD_ATTENTION(
         model_dim = 16,
         n_blocks = 6,
         n_heads = 8,
-        max_length = 200,
+        max_length = 2000, #maximum number of loci in a sample
         activation_function = "ReLU")
 
 
@@ -47,15 +101,16 @@ model.add(Conv1DTranspose(64, 3, activation="relu", padding="same"))
 model.add(Conv1DTranspose(32, 3, activation="relu", padding="same"))
 model.add(Conv1DTranspose(32, 3, activation="relu", padding="same"))
 model.add(Conv1DTranspose(32, 3, activation="relu", padding="same"))
-model.add(Conv1D(5, 3, activation="sigmoid", padding="same"))
+model.add(Conv1D(3, 3, activation="sigmoid", padding="same"))
 
 #Autoencoder
 model.compile(optimizer="adam", loss="mse")
 
 
 #Autoencoder summary
-print("Input Shape: ", specify_input_shape)
+print("Input Shape: ", input_shape)
 model.fit(test_input, test_input, epochs=10)
+
 #save model summary
 with open('modelsummary.txt', 'w') as f:
     model.summary(print_fn=lambda x: f.write(x + '\n'))
@@ -65,7 +120,11 @@ with open('modelsummary.txt', 'w') as f:
 #plot_model(model, to_file='demo.png', show_shapes=True)
 
 
-# In[42]:
+
+
+
+
+
 
 
 #RESIDUAL_CONVOLUTION_1D
@@ -95,15 +154,20 @@ model.add(Conv1DTranspose(64, 3, activation="relu", padding="same"))
 model.add(Conv1DTranspose(32, 3, activation="relu", padding="same"))
 model.add(Conv1DTranspose(32, 3, activation="relu", padding="same"))
 model.add(Conv1DTranspose(32, 3, activation="relu", padding="same"))
-model.add(Conv1D(5, 3, activation="sigmoid", padding="same"))
+model.add(Conv1D(3, 3, activation="sigmoid", padding="same"))
 
 #Autoencoder
 model.compile(optimizer="adam", loss="mse")
 
 
 #Autoencoder summary
-print("Input Shape: ", specify_input_shape)
+print("Input Shape: ", input_shape)
 model.fit(test_input, test_input, epochs=10)
 #save model summary
 with open('modelsummary.txt', 'w') as f:
     model.summary(print_fn=lambda x: f.write(x + '\n'))
+
+
+#show model diagram
+#plot_model(model, to_file='demo.png', show_shapes=True)
+
